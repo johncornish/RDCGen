4f218cf8bea2d55d0f15f75b5b0844ba8a7fb02b
John Cornish <cornish@mymail.mines.edu>
2017-10-20 11:21:08 -0600

    Either I'm stupid or ldap3 is unintuitive...

--- rdgen.py
+++ rdgen.py
@@ -1,5 +1,6 @@
-import click, os
-from ldap3 import Server, Connection, ALL, NTLM
+import click, os, itertools, pprint
+from ldap3 import Server, Connection, NTLM, ALL
+from ldap3 import ObjectDef, AttrDef, Reader
 
 usr = 'ADIT\\cornish'
 psswd = os.environ['PSS']
@@ -7,11 +8,30 @@ psswd = os.environ['PSS']
 
 server = Server('corsair.adit.mines.edu', use_ssl=True, get_info=ALL)
 with Connection(server, user=usr, password=psswd, authentication=NTLM, auto_bind=True) as conn:
-    conn.search('ou=classrooms-win10,ou=csm computers,dc=adit,dc=mines,dc=edu', '(objectclass=computer)')
-    entry = conn.entries[0]
-    data = entry.entry_to_json()
-    print(data)
-    #print([e.entry_to_json() for e in conn.entries])
+    # conn.search('ou=classrooms-win10,ou=csm computers,dc=adit,dc=mines,dc=edu', '(objectclass=computer)')
+    # print(conn.entries)
+
+    computer = ObjectDef('computer', conn)
+    computer += 'cn'
+    computer += 'ou'
+
+    #r = Reader(conn, computer, base='ou=classrooms-win10,ou=csm computers,dc=adit,dc=mines,dc=edu', sub_tree=False, query='type: computer')
+    r = Reader(conn, computer, 'ou=classrooms-win10,ou=csm computers,dc=adit,dc=mines,dc=edu', '')
+    #r.search_subtree(paged_size=5)
+    pp = pprint.PrettyPrinter(indent=4)
+    r.search_paged(paged_size=5)
+    print(len(r.entries))
+    for computer in itertools.islice(r.entries, 1):
+        print(computer)
+        #print(computer)
+        #pp.pprint(computer['attributes'].keys())
+        # print(computer.cn)
+        # print([ou for ou in computer])
+
+    # entry = r.entries
+    # data = entry.entry_to_json()
+    # print(entry)
+    # print([e.entry_to_json() for e in conn.entries])
     # print(conn.extend.standard.who_am_i())
     # print(conn)
 
