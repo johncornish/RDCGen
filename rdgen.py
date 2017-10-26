be6164b4d36e88b97caa5ec0c8b0abf3aca1a7d6
John Cornish <cornish@mymail.mines.edu>
2017-10-26 13:28:20 -0600

    Turns out there's built in support for this

--- rdgen.py
+++ rdgen.py
@@ -3,33 +3,24 @@ from ldap3 import Server, Connection, NTLM, ALL
 from ldap3 import ObjectDef, AttrDef, Reader
 
 usr = "ADIT\\{}".format(click.prompt('Username'))
-
 psswd = os.environ['PSS']
 #psswd = click.prompt('Password', hide_input=True)
 
-## Search is limited by server to 1000 results, but we have 1200+, so breaking up search to next OU level
-server = Server('corsair.adit.mines.edu', use_ssl=True, get_info=ALL)
-with Connection(server, user=usr, password=psswd, authentication=NTLM, auto_bind=True, read_only=True) as conn:
-    conn.search('ou=builds,ou=classrooms-win10,ou=csm computers,dc=adit,dc=mines,dc=edu', '(objectclass=computer)')
-    print(conn.entries)
-    conn.search('ou=case,ou=classrooms-win10,ou=csm computers,dc=adit,dc=mines,dc=edu', '(objectclass=computer)')
-    print(conn.entries)
-    conn.search('ou=ccit,ou=classrooms-win10,ou=csm computers,dc=adit,dc=mines,dc=edu', '(objectclass=computer)')
-    print(conn.entries)
-    conn.search('ou=cecs,ou=classrooms-win10,ou=csm computers,dc=adit,dc=mines,dc=edu', '(objectclass=computer)')
-    print(conn.entries)
-    conn.search('ou=cerse,ou=classrooms-win10,ou=csm computers,dc=adit,dc=mines,dc=edu', '(objectclass=computer)')
-    print(conn.entries)
-    conn.search('ou=hr,ou=classrooms-win10,ou=csm computers,dc=adit,dc=mines,dc=edu', '(objectclass=computer)')
-    print(conn.entries)
-    conn.search('ou=kiosks,ou=classrooms-win10,ou=csm computers,dc=adit,dc=mines,dc=edu', '(objectclass=computer)')
-    print(conn.entries)
-    conn.search('ou=lb,ou=classrooms-win10,ou=csm computers,dc=adit,dc=mines,dc=edu', '(objectclass=computer)')
-    print(conn.entries)
-    conn.search('ou=podiums,ou=classrooms-win10,ou=csm computers,dc=adit,dc=mines,dc=edu', '(objectclass=computer)')
-    print(conn.entries)
-    # ^^use this, the cursor sucks
+def get_computers():
+    ## Search is limited by server to 1000 results, but we have 1200+, so breaking up search to next OU level
+    server = Server('corsair.adit.mines.edu', use_ssl=True, get_info=ALL)
+    with Connection(server, user=usr, password=psswd, authentication=NTLM, auto_bind=True, read_only=True) as conn:
+        entry_generator = conn.extend.standard.paged_search(
+            search_base = 'ou=classrooms-win10,ou=csm computers,dc=adit,dc=mines,dc=edu',
+            search_filter = '(objectclass=computer)',
+            generator=True
+        )
+        return entry_generator
 
+computers = get_computers()
+for c in computers:
+    print(c)
+    break
 ## Debugging notes:
 ## 
 ## Install requirements
